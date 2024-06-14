10ccaa6d6cc11f3b58d86d078a553bdb8e21310a
mendo olame leo <leomendoolame@gmail.com>
2024-06-14 11:52:32 +0100

    derniere modification

--- hello_easynmt.py
+++
@@ -1,9 +0,0 @@
-from easynmt import EasyNMT
-
-model= EasyNMT('opus-mt')
-
-sentences = ['In dieser Liste definieren wir mehrere Sätze.']
-text= ['leo aime les belles filles']
-translations = model.translate(text, target_lang='en')
-
-print(f'the translation is:{translations}%')
